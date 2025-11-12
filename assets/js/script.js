72836a9afc44c847adafdd122e8934ffbcbc8d6a
svanrell <svanrell@alumnes.santjosepobrer.es>
2025-11-12 20:36:00 +0100

    Añadir función al cargar la página para botones de login y registro

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.container');
+    const toSignup = document.getElementById('toSignup');
+    const toLogin = document.getElementById('toLogin');
+
+
+
+
+
+
+
+});
